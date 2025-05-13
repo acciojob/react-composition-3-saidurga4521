209c62a827d1ff1635c6da310df3553d5e299b1d
Durga Prasad <saikamireddi3@gmail.com>
2025-05-13 12:36:29 +0530

    changes has done

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ const App = () => {
   return (
     <div>
       <Tooltip text="This is a tooltip for Button">
-        <h1>Hover me</h1>
+        <h1 className="tooltip">Hover me</h1>
       </Tooltip>
 
       <br />
       <br />
 
       <Tooltip text="Tooltip for a paragraph">
-        <p>Hover me see to another tooltip</p>
+        <p className="tooltip">Hover me see to another tooltip</p>
       </Tooltip>
     </div>
   );
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,14 +3,13 @@ import React, { useState } from "react";
 const Tooltip = ({ text, children }) => {
   const [isVisible, setIsVisible] = useState(false);
   return (
-    <h2
-      className="tooltip"
+    <div
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
     >
       {children}
       {isVisible && <div className="tooltiptext">{text}</div>}
-    </h2>
+    </div>
   );
 };
 
